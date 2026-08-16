a9422fd7aa1e5a9e0ddc914f2c5613bde3127374
Deepiga0706 <deepiga.rs2024cce@sece.ac.in>
2026-08-16 18:24:44 +0530

    problem 3 problems by my own

---
+++ leet_code/arrays/length_of_last_word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+       int n=s.size();
+       int ans=0;
+       for(int i=n-1;i>=0;i--){
+        if(s[i]==' '&&ans!=0){
+            return ans;
+            break;
+        }else if(s[i]!=' '){
+        ans++;
+        }
+       } 
+       return ans;
+    }
+};
